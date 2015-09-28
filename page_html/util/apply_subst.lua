c483a86dab001cf85a97e95c209cda694d5de40c
Jasper den Ouden <o.jasper@gmail.com>
2015-09-28 17:22:23 +0200

    Some ability to put in arguments.

--- page_html/util/apply_subst.lua
+++ page_html/util/apply_subst.lua
@@ -1,18 +1,21 @@
 return function(str, subst)
    assert(type(str) == "string")
    local fail_n = 0
-   local function fun(key)
-      if subst[key] then
-         return subst[key]
-      else
+   local function fun(key, args)
+      local got = subst[key]
+      if not got then
          fail_n = fail_n + 1
+      elseif type(got) == "function" then
+         return got(args)
+      else
+         return got
       end
    end
 
    local n, k = 1, 0
    while n > fail_n and k < 256 do  -- More items substituted than failed.
       fail_n = 0
-      str, n = string.gsub(str, "{%%([_.:/%w%%]+)}", fun)
+      str, n = string.gsub(str, "{%%([_.:/%w%%]+)|?([_.,%s:/%w%%]*)}", fun)
       k = k + 1
    end
    return str
