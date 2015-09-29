4618134e3baa373e787631c70dee9acfb8414c90
Jasper den Ouden <o.jasper@gmail.com>
2015-09-29 02:23:57 +0200

    add `max_tries` argument, and slight rearrangement

--- page_html/util/apply_subst.lua
+++ page_html/util/apply_subst.lua
@@ -1,18 +1,16 @@
-return function(str, subst)
+return function(str, subst, max_tries)
    assert(type(str) == "string")
    local fail_n = 0
    local function fun(key, args)
       local got = subst[key]
       if type(got) == "function" then got = got(args) end
-      if got ~= nil then
-         return got
-      else
-         fail_n = fail_n + 1
-      end
+
+      if got == nil then fail_n = fail_n + 1 end
+      return got
    end
 
    local n, k = 1, 0
-   while n > fail_n and k < 256 do  -- More items substituted than failed.
+   while n > fail_n and k < (max_tries or 256) do  -- More items substituted than failed.
       fail_n = 0
       str, n = string.gsub(str, "{%%([_.:/%w%%]+)|?([_.,%s:/%w%%]*)}", fun)
       k = k + 1
