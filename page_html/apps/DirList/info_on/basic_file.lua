4fd2b6dcb8e401abbf02a4fa90348c29e292221c
Jasper den Ouden <o.jasper@gmail.com>
2015-09-29 16:33:27 +0200

    stuff

--- page_html/apps/DirList/info_on/basic_file.lua
+++ page_html/apps/DirList/info_on/basic_file.lua
@@ -6,7 +6,17 @@ local lfs = require "lfs"
 local This = {}
 This.__index = This
 
-function This:new(new) return setmetatable(new, self) end
+This.name = "basic_file"
+
+function This:new(new)
+   new = setmetatable(new, self)
+   new:init()
+   return new
+end
+
+function This:init()
+   self.attrs = lfs.attributes(self.dir .. "/" .. self.file)
+end
 
 function This:priority()
    return 0
@@ -18,18 +28,18 @@ This.head = "<td>{%mode}</td><td>{%dir}</td><td>{%file}</td>"
 local apply_subst = require "page_html.util.apply_subst"
 
 function This:output()  -- TODO not allowed to touch local shit..
-   local fp = self.dir .. "/" .. self.file
-   local ret, attrs = {}, lfs.attributes(fp) or {}
-   attrs.file = self.file
-   attrs.dir  = self.dir
-   attrs.mode = attrs.mode == "directory" and "dir" or attrs.mode
+   local ret, repl = {}, self.attrs or {}
+   repl.file = self.file
+   repl.dir  = self.dir
 
-   table.insert(ret, apply_subst(This.head, attrs))
+   repl.mode = self.mode == "directory" and "dir" or self.mode
 
-   attrs.file = nil
-   attrs.dir = nil
-   attrs.mode = nil
-   for k,v in pairs(attrs) do
+   table.insert(ret, apply_subst(self.head, repl))
+
+   repl.file = nil
+   repl.dir = nil
+   repl.mode = nil
+   for k,v in pairs(repl) do
       table.insert(ret, string.format(self.other_format, 
                                       k,v))
    end
