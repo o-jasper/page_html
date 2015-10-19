15ece816c633235e4a4399ff4de51df1ebfe0454
Jasper den Ouden <o.jasper@gmail.com>
2015-10-20 00:31:23 +0200

    Comply with how javascript bindings are done now. Change how `at_dir` works.

--- page_html/apps/DirList/init.lua
+++ page_html/apps/DirList/init.lua
@@ -18,11 +18,12 @@ This.db_filename = ":memory:"
 function This:init()
    self.dir_sql = self.Dir:new{filename=self.db_filename}
    assert(self.dir_sql)
-   self:to_dir(self.at_dir)
+
+   self.at_dir = self.at_dir or os.getenv("HOME")
 end
 
 function This:to_dir(path)
-   local path = path or self.at_dir or os.getenv("HOME")
+   local path = path or self.at_dir
    self.dir_sql:update_directory(path)
    self.at_dir = path
 
@@ -42,43 +43,42 @@ This.info_ons = {
    require "page_html.apps.DirList.info_on.file"
 }
 
-local rpc_js = {}
-This.rpc_js = rpc_js
+This.rpc_js = {}
 
-function rpc_js:to_dir()
-   return function(path)
-      self.rpc_js.search(self)("", { to_dir=path })
-   end
+function This.rpc_js:to_dir(path)
+   self.rpc_js.search(self)("", { to_dir=path })
 end
 
 local ret_list = require "page_html.apps.lib.ret_list"
 local info_on  = require "page_html.info_on"
 -- TODO defaultly need a current_directory..
-function rpc_js:search()
+function This.rpc_js:search(term, state, ...)
    assert(self.dir_sql)
-   return function(term, state, ...)
-      state.to_dir = state.to_dir or self.at_dir or os.getenv("HOME")
-      self:to_dir(state.to_dir)
+   print("----------------------")
+   state.to_dir = state.to_dir or self.at_dir or os.getenv("HOME")
 
-      local form = search(self.Formulator, self.allow_direct)(term, state, ...)
-      form:equal("dir", state.to_dir)
+   self:to_dir(state.to_dir)
 
-      local list = self.dir_sql:exec(form:sql_pattern(), unpack(form:sql_values()))
-      local info_list = info_on.list(list, self, self.info_ons)
+   local form = search(self.Formulator, self.allow_direct)(term, state, ...)
+   form:equal("dir", state.to_dir)
 
-      -- TODO might want to select higher-priority ones for each entry.
+   print("Q", form:sql_pattern())
+   local list = nil
+   pcall(function()
+         list = self.dir_sql:exec(form:sql_pattern(), unpack(form:sql_values()))
+   end)  -- TODO handle failure?
+   local info_list = list and info_on.list(list, self, self.info_ons) or {}
 
-      state.where = self.where
-      return ret_list(info_list, state)
-   end
+   -- TODO might want to select higher-priority ones for each entry.
+   state.where = self.where
+   return ret_list(info_list, state)
 end
 
 function This:repl(state)
-   local path = state.rest_path
-   if not string.find(path, "^/") then
-      path = os.getenv("HOME") .. "/" .. path
+   self.at_dir = state.rest_path or "/"
+   if not string.find(self.at_dir, "^/") then
+      self.at_dir = os.getenv("HOME") .. "/" .. self.at_dir
    end
-   self:to_dir(path)
    return {}
 end
 
