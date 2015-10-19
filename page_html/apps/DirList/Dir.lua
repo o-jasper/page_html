149a472d1a73bc6cf0fd391e38b0ed6855a2830a
Jasper den Ouden <o.jasper@gmail.com>
2015-10-20 00:30:24 +0200

    Fixes and `;` in sql

--- page_html/apps/DirList/Dir.lua
+++ page_html/apps/DirList/Dir.lua
@@ -34,17 +34,20 @@ This.repl = { table_name = "files" }
 local cmd_strs = {}
 This.cmd_strs = cmd_strs
 
-cmd_strs.select_dir  = "SELECT * FROM {%table_name} WHERE dir == ?"
-cmd_strs.select_path = "SELECT id FROM {%table_name} WHERE dir == ? AND file == ?"
-cmd_strs.delete_path = "DELETE FROM {%table_name} WHERE dir == ? AND file == ?"
+cmd_strs.select_dir  = "SELECT * FROM {%table_name} WHERE dir == ?;"
+cmd_strs.select_path = "SELECT id FROM {%table_name} WHERE dir == ? AND file == ?;"
+cmd_strs.delete_path = "DELETE FROM {%table_name} WHERE dir == ? AND file == ?;"
 
--- Enter using the autoincrement.
-cmd_strs.enter       = "INSERT INTO {%table_name} VALUES (NULL, ?,?,?, ?,?,?)"
+-- Enter using the autoincrement.(assuming not already exists)
+cmd_strs.enter   = "INSERT INTO {%table_name} VALUES (NULL, ?,?,?, ?,?,?);"
+-- (not assuming already exists)
+-- TODO doesnt work for somer reason? Deletes itself?
+cmd_strs.enter_replace = cmd_strs.delete_path .. cmd_strs.enter
 
 function This:enter(entry)
    -- Delete pre-existing.
-   self:cmd("delete_path")(entry.dir, entry.file)
    if entry then
+      self:cmd("delete_path")(entry.dir, entry.file)
       assert(not entry.id) -- Re enter.,
       self:cmd("enter")(entry.dir, entry.file, entry.mode,
                         entry.size, entry.access, entry.modification)
@@ -68,12 +71,14 @@ local gettime = require("socket").gettime
 function This:update_directory(directory)
    local update_times = self.update_times
    -- Remove gone stale.
-   while #update_times > 0 and update_times[1][1] - gettime() > self.fresh_time do
+   while #update_times > 0 and gettime() - update_times[1][1] > self.fresh_time do
       table.remove(update_times, 1)
    end
+
    for _, el in ipairs(update_times) do
       if el[2] == directory then return true end  -- In there, fresh.
    end
+
    table.insert(update_times, {gettime(), directory})
 
    local fd = io.open(directory)
