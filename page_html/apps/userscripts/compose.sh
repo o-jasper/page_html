de9c6b1e1c654047416ec8275aea9cab2de782cc
Jasper den Ouden <o.jasper@gmail.com>
2016-02-22 17:00:54 +0100

    Keep whitespace infront  (bash scripting sucks)

--- page_html/apps/userscripts/compose.sh
+++ page_html/apps/userscripts/compose.sh
@@ -4,18 +4,18 @@ echo "// In current case not preferred form of editing!"
 echo "// As of 2-2016 see github.com/o-jasper/page_html"
 echo
 
-cat $1 | while read line; do
+cat $1 | while IFS='' read -r line; do  # Shell sucks.
     INSERT=$(echo $line | tail -c+4)
     case "$line" in
         =a=*)
             echo // -insert-asset  $INSERT
             cat ../../assets/$INSERT
-            echo ;;
+            echo // -end ;;
         =s=*)
             echo // -insert-source $INSERT
             cat src/$INSERT
-            echo ;;
+            echo // -end ;;
         *)
-            echo "$line" ;;
+            echo -e "$line" ;;
     esac
 done
