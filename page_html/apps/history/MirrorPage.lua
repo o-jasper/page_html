994d8b095f5c944d22e46101c7fe35633b3c9cc9
Jasper den Ouden <o.jasper@gmail.com>
2016-02-29 18:52:21 +0100

    minor

--- page_html/apps/history/MirrorPage.lua
+++ page_html/apps/history/MirrorPage.lua
@@ -13,12 +13,17 @@ local lfs = require "lfs"
 
 local apply_subst = require "page_html.util.apply_subst"
 This.mirror_msg = {
-      pattern = [[Mirror:{%is_top}<br><br>]],
+      pattern = [[Excepting outside assets, this a local mirror:{%is_top}<br><br>]],
       is_top = [[<span style="color:gray">(top of dir)</span>]],
 }
 
 function This:output(args)
    local mm = self.mirror_msg
+
+   local function finally(ret, data)
+      -- Can we indicate that no scripts are to be ran?
+      return apply_subst(mm.pattern, data) .. ret
+   end
 
    local file = self.dir .. args.rest_path
    if lfs.attributes(file, "mode") == "directory" then
@@ -26,7 +31,7 @@ function This:output(args)
       if fd then
          local ret = fd:read("*a")
          fd:close()
-         return apply_subst(mm.pattern, {is_top=mm.is_top}) .. ret
+         return finally(ret, {is_top=mm.is_top})
       else
          return "Is a directory with no index.html file. TODO redirect to directory view?"
       end
@@ -39,7 +44,7 @@ function This:output(args)
       if fd then
          local ret, msg, code = fd:read("*a")
          if ret then
-            return apply_subst(mm.pattern, {is_top=" "}) .. ret
+            return finally(ret, {is_top=" "})
          else
             return report("Couldnt read", msg, code)
          end
