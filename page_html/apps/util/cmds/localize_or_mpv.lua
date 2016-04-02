73dd1df7ec11aba204cbefc2e5ce2d70b047cbe3
Jasper den Ouden <o.jasper@gmail.com>
2016-04-03 01:58:47 +0200

    Clearer difference, should view at right time.

--- page_html/apps/util/cmds/localize_or_mpv.lua
+++ page_html/apps/util/cmds/localize_or_mpv.lua
@@ -25,12 +25,13 @@ return function(server, uri, ...)
       local mirror_uri, success =
          server.pages.history_mirrored:mirror_uri(uri, mirror_on_userscript)
 
+      local ret = {m_uri=mirror_uri, view_it=success and view_it,
+                   pref_uri = (m and (mirror_uri .. "/html/")) or nil}
       if success then  -- Got it.
-         return {m_uri=mirror_uri, view_it=success and view_it,
-                 pref_uri = (m and (mirror_uri .. "/html/")) or nil
-         }
+         return ret
       elseif view_it then  -- Don't have it and need to get from the userscript.
-         return { m_uri=mirror_uri, get_it=mirror_on_userscript, view_it=view_it }
+         ret.get_it = mirror_on_userscript
+         return ret
       end
    else  -- Straight video.
       mpv(nil, uri, ...)  -- Shouldnt care about server.
