79776b91be436709517fb1b7edae1c5ce874e968
Jasper den Ouden <o.jasper@gmail.com>
2016-04-28 12:15:17 +0200

    Tweak uri a bit

--- page_html/apps/util/cmds/mpv.lua
+++ page_html/apps/util/cmds/mpv.lua
@@ -3,11 +3,12 @@ local default_geometry = "100%x100%"
 -- TODO video-finished callback..
 
 function mpv_cmd(uri, geometry)
+   local use_uri = string.match(uri, "^[^&]+") or uri
    if geometry == "fullscreen" then
-      return string.format("mpv --force-window --fs \"%s\" &", uri)
+      return string.format("mpv --force-window --fs \"%s\" &", use_uri)
    else
       return string.format("mpv --force-window --geometry=%s \"%s\" &",
-                          geometry or default_geometry, uri)
+                          geometry or default_geometry, use_uri)
    end
 end
 
