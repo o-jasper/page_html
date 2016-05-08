29af077e4c8a8ecd52fef0fecdb694e0f4195644
Jasper den Ouden <o.jasper@gmail.com>
2016-05-08 17:55:50 +0200

    Allow dot in apply_subst too.

--- page_html/util/apply_subst.lua
+++ page_html/util/apply_subst.lua
@@ -1,4 +1,4 @@
-return function(str, subst, max_tries)
+return function(str, subst, max_tries, matcher)
    assert(type(str) == "string", string.format("Subst not string instead: %s", str))
    local fail_n = 0
    local function fun(key, args)
@@ -12,7 +12,7 @@ return function(str, subst, max_tries)
    local n, k = 1, 0
    while n > fail_n and k < (max_tries or 256) do  -- More items substituted than failed.
       fail_n = 0
-      str, n = string.gsub(str, "{%%([%w_]+)[%s]*([^}]*)}", fun)
+      str, n = string.gsub(str, matcher or "{%%([%w_./]+)[%s]*([^}]*)}", fun)
       k = k + 1
    end
    return str
