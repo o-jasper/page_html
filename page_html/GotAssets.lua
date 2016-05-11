ad4d3f37c0a1ec27a8ee19bc72c2b28c3822f024
Jasper den Ouden <o.jasper@gmail.com>
2016-05-12 00:08:30 +0200

    Add default user override location for assets

--- page_html/GotAssets.lua
+++ page_html/GotAssets.lua
@@ -3,6 +3,13 @@ local This = require("page_html.util.Class"):class_derive{ __name="page_html.Got
 This.Assets = require "page_html.Assets"
 function This:init()
    self.assets_arg = self.assets_args or {where = self.where}
+
+   if self.data_dir and self.name then
+      -- Place user overrides can go.
+      table.insert(self.assets_arg.where, 1,
+                   self.data_dir .. "assets/" .. self.name .. "/")
+   end
+
    self.assets = self.Assets:new(self.assets_arg)
 end
 
