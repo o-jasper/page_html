24094668974c63aa9c1b8a00106d8a2da9393fba
Jasper den Ouden <o.jasper@gmail.com>
2016-05-17 15:29:04 +0200

    Thats not an argument here, now it should torify

--- page_html/apps/util/cmds/mirror.lua
+++ page_html/apps/util/cmds/mirror.lua
@@ -1,6 +1,6 @@
 local mirror_image = require "page_html.apps.util.mirror_image"
 
-local mirror_on_userscript, view_it = false, true
+local view_it = true
 
 return function(info, uri, ...)
    local ret = mirror_image{}(server, uri, ...)
@@ -8,7 +8,7 @@ return function(info, uri, ...)
    -- Mirror it as a page.
    local mirrorer = info.server.pages.history_mirrored
    local cmd_name = string.match(info.extra or "", [[;tor=true;]]) and "tor_wget_kr"
-   local mirror_uri = mirrorer:mirror_uri_kr(uri, mirror_on_userscript, cmd_name)
+   local mirror_uri = mirrorer:mirror_uri_kr(uri, cmd_name)
 
    return { m_uri=mirror_uri, view_it=view_it }
 end
